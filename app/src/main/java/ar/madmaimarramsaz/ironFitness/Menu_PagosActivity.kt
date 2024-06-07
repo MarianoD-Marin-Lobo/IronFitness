e6c4af2f9b671621c49bfbee5570050a45cd78f7
mc780 <119272523+mc780@users.noreply.github.com>
2024-06-07 19:45:22 -0300

    .
    .

--- app/src/main/java/ar/madmaimarramsaz/ironFitness/Menu_PagosActivity.kt
+++ app/src/main/java/ar/madmaimarramsaz/ironFitness/Menu_PagosActivity.kt
@@ -5,6 +5,7 @@ import android.graphics.Color
 import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -13,6 +14,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Menu_PagosActivity : AppCompatActivity() {
+    private lateinit var btnPagoMensualSocio: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
