9807fffc687a9cdfe12dc5cb9f42a4a7f4e9412d
mc780 <119272523+mc780@users.noreply.github.com>
2024-06-14 11:21:02 -0300

    ventanas nuevo pago
    ventanas nuevo pago

--- app/src/main/java/ar/madmaimarramsaz/ironFitness/Menu_PagosActivity.kt
+++ app/src/main/java/ar/madmaimarramsaz/ironFitness/Menu_PagosActivity.kt
@@ -1,34 +1,45 @@
 package ar.madmaimarramsaz.ironFitness
 
 import android.content.Intent
-import android.graphics.Color
-import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class Menu_PagosActivity : AppCompatActivity() {
     private lateinit var btnPagoMensualSocio: Button
+    private lateinit var btnPagoActividadDiaria: Button
+    private lateinit var btnVerHistorialPagos: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_menu_pagos)
 
+
         btnPagoMensualSocio = findViewById(R.id.menu_pagos_pago_cuota_mensual_socio_button)
+        btnPagoActividadDiaria = findViewById(R.id.menu_pagos_pago_activ_diaria_button)
+        btnVerHistorialPagos = findViewById(R.id.menu_pagos_historial_pagos_button)
 
         // Boton de pago cuota Mensual
         btnPagoMensualSocio.setOnClickListener {
+            // Se abre la ventana a la pagina de pagos
+            val intent = Intent(this, Nuevo_Pago_Pag1_Activity::class.java)
+            startActivity(intent)
+        }
 
-        // Se abre la ventana al home
-        val intent = Intent(this, Menu_PagosActivity::class.java)
-        startActivity(intent)
+        // Boton de pago Actividad Diaria
+        btnPagoActividadDiaria.setOnClickListener {
+            // Se abre la ventana a la pagina de pagos
+            val intent = Intent(this, Nuevo_Pago_Pag1_Activity::class.java)
+            startActivity(intent)
+        }
+
+        // Boton Ver Historial de Pagos
+        btnVerHistorialPagos.setOnClickListener {
+            // Se abre la ventana al Historial de Pagos
+            val intent = Intent(this, Historial_Pagos_Activity::class.java)
+            startActivity(intent)
         }
     }
 }
