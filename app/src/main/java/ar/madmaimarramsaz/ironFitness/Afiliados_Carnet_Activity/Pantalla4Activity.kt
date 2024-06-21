67ed51beac5ea41f41a1bbba5ef8618d9d1bcb68
mc780 <119272523+mc780@users.noreply.github.com>
2024-06-21 14:57:07 -0300

    botones gestion afiliados
    .

--- app/src/main/java/ar/madmaimarramsaz/ironFitness/Afiliados_Carnet_Activity/Pantalla4Activity.kt
+++ app/src/main/java/ar/madmaimarramsaz/ironFitness/Afiliados_Carnet_Activity/Pantalla4Activity.kt
@@ -1,10 +1,15 @@
 package ar.madmaimarramsaz.ironFitness.Afiliados_Carnet_Activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import ar.madmaimarramsaz.ironFitness.GestionAfiliadoActivity
+import ar.madmaimarramsaz.ironFitness.HomeActivity
+import ar.madmaimarramsaz.ironFitness.Menu_PagosActivity
 import ar.madmaimarramsaz.ironFitness.R
 
 class Pantalla4Activity : AppCompatActivity() {
@@ -12,10 +17,39 @@ class Pantalla4Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_pantalla4)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // boton volver a la ventana anterior
+        val btn_volver: Button = findViewById(R.id.image_back_button)
+
+        btn_volver.setOnClickListener {
+            finish()
+        }
+
+        // barra de navegación Botón 1 Pagar
+
+        val btn_pagar: Button = findViewById(R.id.image_low_menu_quad)
+
+        btn_pagar.setOnClickListener {
+            val intent = Intent(this, Menu_PagosActivity::class.java)
+            startActivity(intent)
+        }
+
+        // barra de navegación Botón 2 ir a Home
+
+        val btn_home: Button = findViewById(R.id.image_low_menu_quad1)
+
+        btn_home.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        // barra de navegación Botón 3 ir a Gestión Afiliados
+
+        val btn_gestionAfiliado: Button = findViewById(R.id.image_low_menu_quad2)
+
+        btn_gestionAfiliado.setOnClickListener {
+            val intent = Intent(this, GestionAfiliadoActivity::class.java)
+            startActivity(intent)
+        }
         }
     }
-}
--- app/src/main/java/ar/madmaimarramsaz/ironFitness/GestionAfiliadoActivity.kt
+++ app/src/main/java/ar/madmaimarramsaz/ironFitness/GestionAfiliadoActivity.kt
@@ -1,8 +1,9 @@
 package ar.madmaimarramsaz.ironFitness
 
-
+import android.content.Intent
 import android.graphics.Matrix
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -10,6 +11,10 @@ import androidx.core.graphics.rotationMatrix
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
+import ar.madmaimarramsaz.ironFitness.GestionAfiliadoActivity
+import ar.madmaimarramsaz.ironFitness.HomeActivity
+import ar.madmaimarramsaz.ironFitness.Menu_PagosActivity
+import ar.madmaimarramsaz.ironFitness.R
 
 
 class GestionAfiliadoActivity : AppCompatActivity() {
@@ -32,5 +37,28 @@ class GestionAfiliadoActivity : AppCompatActivity() {
                 else -> null
             }
         }.attach()
+
+        // boton volver a la ventana anterior
+        val btn_volver: Button = findViewById(R.id.image_back_button)
+
+        btn_volver.setOnClickListener {
+            finish()
+        }
+
+        // barra de navegación Botón 1 Pagar
+        val btn_pagar: Button = findViewById(R.id.image_low_menu_quad)
+
+        btn_pagar.setOnClickListener {
+            val intent = Intent(this, Menu_PagosActivity::class.java)
+            startActivity(intent)
+        }
+
+        // barra de navegación Botón 3 ir a Home
+        val btn_home: Button = findViewById(R.id.image_low_menu_quad2)
+
+        btn_home.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
